11a825fb262412863f5e6189cc9743355275ffe6
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-08-10 18:56:06 -0300

    First commit

---
+++ backend/src/main/java/com/ios/arj/ArjApplication.java
@@ -0,0 +1,13 @@
+package com.ios.arj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ios/arj/ArjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ios.arj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
